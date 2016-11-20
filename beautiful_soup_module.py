69d0ec86cb6461a766439f21f3afe54fdf6b9de7
Sriram Sreedhar <sriram003@gmail.com>
2016-11-19 22:18:21 -0800

    Update beautiful_soup_module.py

--- beautiful_soup_module.py
+++ beautiful_soup_module.py
@@ -6,8 +6,17 @@ from bs4 import BeautifulSoup
 
 html = urlopen("http://www.ndtv.com")
 bsObj = BeautifulSoup(html.read(),"html.parser")
-print(bsObj.h1)
-print(bsObj.h2)
-print(bsObj.h3)
-print(bsObj.h4)
-print(bsObj.body)
+#print(bsObj.h1)
+#print(bsObj.h2)
+#print(bsObj.h3)
+#print(bsObj.h4)
+#print(bsObj.body)
+#view just title of website
+print(bsObj.title)
+
+
+
+#Result looks like
+#sh-3.2# python beautiful_soup.py
+#<title> 	NDTV: Latest News, India News, Breaking News, Business, Bollywood, Cricket, Videos &amp; Photos </title>
+#sh-3.2#
